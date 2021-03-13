0c8fd482524ec031d838b72e75b10ff6dc159f46
Kritsatorn Saengweang <kritsatornsaengweang@MacBook-Pro-4.local>
2021-03-13 11:33:19 +0700

    setup prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App ">
       <header className="App-header">
-      
-      <div className=""></div>
+        <div className=" w-full h-screen bg-green-300 text-gray-300 text-center ">
+          HELLLO <span>HdsdsE</span>
+        </div>
       </header>
     </div>
   );
